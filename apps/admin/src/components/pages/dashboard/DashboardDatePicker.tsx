83f3c29e00bcaf03a9f8f4a6bdee36169d3b7510
HPhat03 <107537439+HPhat03@users.noreply.github.com>
2025-12-08 15:31:41 +0700

    Refactor: Class name refactor 2

--- apps/admin/src/components/pages/dashboard/DashboardDatePicker.tsx
+++ apps/admin/src/components/pages/dashboard/DashboardDatePicker.tsx
@@ -50,7 +50,7 @@ const DashboardDatePicker: FC<DashboardDatePickerProps> = ({ dateRange, setDateR
         <PopoverTrigger asChild>
           <Button
             variant="outline"
-            className="flex items-center gap-2 rounded-md border bg-white text-sm font-normal text-gray-700 shadow-sm"
+            className="text-base-foreground flex items-center gap-2 rounded-md border bg-white text-sm font-normal shadow-sm"
           >
             <CalendarIcon />
             {displayDateRange}
