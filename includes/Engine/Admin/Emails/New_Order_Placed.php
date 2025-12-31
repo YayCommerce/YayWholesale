d63c572c880c860fe1ecc425c631fde0791f4385
HPhat03 <107537439+HPhat03@users.noreply.github.com>
2025-12-31 10:39:44 +0700

    Fixed: order status slug name

--- includes/Engine/Admin/Emails/New_Order_Placed.php
+++ includes/Engine/Admin/Emails/New_Order_Placed.php
@@ -146,7 +146,7 @@ class New_Order_Placed extends Wholesale_Email_Base {
             $this->placeholders['{customer_name}'] = $this->object->get_formatted_billing_full_name();
             if ( $this->is_enabled() && $this->get_recipient() ) {
                 // Wholesale order
-                if ( $order->get_meta( '_ywhs_wholesale_role' ) && 'wc-checkout-draft' !== $order->get_status() ) {
+                if ( $order->get_meta( '_ywhs_wholesale_role' ) && 'checkout-draft' !== $order->get_status() ) {
                         $this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
                 }
             }
