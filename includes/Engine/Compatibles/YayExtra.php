4ba6536c8eb24e577cc7ffc651640a5da53c2230
HPhat03 <107537439+HPhat03@users.noreply.github.com>
2026-05-11 17:24:25 +0700

    Compatible: YayExtra (product type)

--- includes/Engine/Compatibles/YayExtra.php
+++ includes/Engine/Compatibles/YayExtra.php
@@ -29,12 +29,32 @@ class YayExtra {
         $script_handle = 'yay-extra-compatible';
 
         $wholesale_role = CustomerHelper::get_current_user_wholesale_role();
-        $product        = wc_get_product( get_the_ID() );
+        $product_id     = get_the_ID();
+        $product        = wc_get_product( $product_id );
+        if ( ! isset( $product ) ) {
+            return;
+        }
 
-        $currency      = apply_filters( 'ywhs_get_currency_by_third_party', [] );
-        $sale_price    = $product->get_sale_price();
-        $regular_price = $product->get_regular_price();
+        // Price
+        $sale_prices    = [];
+        $regular_prices = [];
 
+        $children = $product->get_children();
+        if ( count( $children ) ) {
+            foreach ( $children as $child ) {
+                $child_product            = wc_get_product( $child );
+                $sale_prices[ $child ]    = (float) $child_product->get_sale_price();
+                $regular_prices[ $child ] = (float) $child_product->get_regular_price();
+            }
+        } else {
+            $sale_prices[ $product_id ]    = (float) $product->get_sale_price();
+            $regular_prices[ $product_id ] = (float) $product->get_regular_price();
+        }
+
+        // Currency
+        $currency = apply_filters( 'ywhs_get_currency_by_third_party', [] );
+
+        // Tax
         $tax_class = $product->get_tax_class();
         $tax_rates = WC_Tax::get_rates( $tax_class );
 
@@ -45,15 +65,16 @@ class YayExtra {
             $tax_rate = 0;
         }
 
-        wp_enqueue_script( $script_handle, YAYWHOLESALEB2B_PLUGIN_URL . 'assets/js/yay-extra-compatible.js', $dep, YAYWHOLESALEB2B_VERSION, false );
+        wp_enqueue_script( $script_handle, YAYWHOLESALEB2B_PLUGIN_URL . 'assets/js/yay-extra-compatible.js', $dep, YAYWHOLESALEB2B_VERSION, true );
         wp_localize_script(
             $script_handle,
             'yayWholesaleExtra',
             [
                 'wholesale_role'     => $wholesale_role,
-                'sale_price'         => $sale_price,
-                'regular_price'      => $regular_price,
-                'currency_rate'      => empty( $currency ) ? 1 : $currency['rate'],
+                'product_type'       => $product->get_type(),
+                'sale_prices'        => $sale_prices,
+                'regular_prices'     => $regular_prices,
+                'currency_rate'      => empty( $currency ) || ! isset( $currency['rate'] ) ? 1 : $currency['rate'],
                 // Tax handle
                 'tax_enabled'        => wc_tax_enabled(),
                 'prices_include_tax' => wc_prices_include_tax(),
