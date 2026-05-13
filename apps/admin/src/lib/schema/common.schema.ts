5bea35c450358c48ba814b6a8dcbbd1476eb96b0
Hoang Truong <tvhoang91@gmail.com>
2026-05-13 14:38:36 +0700

    design next feature data

--- apps/admin/src/lib/schema/common.schema.ts
+++ apps/admin/src/lib/schema/common.schema.ts
@@ -1,10 +1,23 @@
 import { z } from 'zod';
 
-/** TODO: apply to payment method, shipping method*/
-export const enableByRoleSchema = z.object({
+// TODO: apply to payment method, shipping method
+const enableByRoleSchema = z.object({
   retailers: z.enum(['enabled', 'disabled']),
-  wholesalers: z.enum(['enabled', 'disabled', 'disabled_specific_roles']),
+  wholesalers: z.enum(['enabled', 'disabled', 'disabled-specific-roles']),
   disabled_specific_roles: z.array(z.string()), // RoleSlug[]
 });
 
-export type EnableByRole = z.infer<typeof enableByRoleSchema>;
+type EnableByRole = z.infer<typeof enableByRoleSchema>;
+
+// TODO: add to settings
+type AccessRestriction = {
+  guest: 'hide-prices' | 'hide-shop' | 'no-restriction';
+  retailers: 'hide-wholesale-prices' | 'show-wholesale-prices';
+};
+
+/**
+ * TODO: remove role.applyToSalePrice
+ */
+type Display = {
+  price_format: 'retail-and-wholesale' | 'wholesale-only' | 'apply-sale-price';
+};
--- includes/Initialize.php
+++ includes/Initialize.php
@@ -14,20 +14,21 @@ class Initialize {
      * The Constructor that load the engine classes
      */
     protected function __construct() {
-        \YayWholesaleB2B\Engine\Hooks::get_instance();
         \YayWholesaleB2B\Engine\RestAPI::get_instance();
-        \YayWholesaleB2B\Engine\Compatibles::get_instance();
         \YayWholesaleB2B\Engine\Register\RegisterFacade::get_instance();
         \YayWholesaleB2B\Engine\Admin\Settings::get_instance();
         \YayWholesaleB2B\Engine\Admin\Users::get_instance();
         \YayWholesaleB2B\Engine\Admin\Orders::get_instance();
         \YayWholesaleB2B\Engine\Admin\Emails\Emails::get_instance();
+
         \YayWholesaleB2B\Engine\Frontend\Frontend::get_instance();
         \YayWholesaleB2B\Engine\Frontend\Pricing::get_instance();
         \YayWholesaleB2B\Engine\Frontend\Coupon::get_instance();
         \YayWholesaleB2B\Engine\Frontend\Tax::get_instance();
         \YayWholesaleB2B\Engine\Frontend\RequestForm::get_instance();
         \YayWholesaleB2B\Engine\Frontend\Requirement::get_instance();
+
+        \YayWholesaleB2B\Engine\Compatibles::get_instance();
         \YayWholesaleB2B\Engine\Support\Support::get_instance();
     }
 }
