23d3ca1fa2f24a26de7578c4df50443efe2c4f84
Hoang Truong <tvhoang91@gmail.com>
2026-05-14 13:38:05 +0700

    fix retailer fatal error

--- apps/admin/src/lib/utils.ts
+++ apps/admin/src/lib/utils.ts
@@ -1,11 +1,16 @@
 import { clsx, type ClassValue } from 'clsx';
-import { HTTPError } from 'ky';
-import { twMerge } from 'tailwind-merge';
-
-import { toast } from '@/components/ui/sonner';
+import { extendTailwindMerge } from 'tailwind-merge';
 
 export const isPro = import.meta.env.VITE_IS_PRO === 'true';
 export const isLite = !isPro;
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      transition: ['transition-default'],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,15 +18,4 @@ export function cn(...inputs: ClassValue[]) {
 
 export const getPagesForWholesaleStore = () => {
   return window.yayWholesaleB2BAdmin.valid_wholesale_store_pages;
-};
-
-export const handleErrorMessage = async (error: Error) => {
-  let errorMessage = error.message ?? 'An Error has occurred!';
-  if (error instanceof HTTPError) {
-    const body = await error.response.json();
-    if (!!body?.message) {
-      errorMessage = body.message;
-    }
-  }
-  toast.error(errorMessage);
 };
