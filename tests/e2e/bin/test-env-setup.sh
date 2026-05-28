c40f621692c34aca4f8a1c36e52a4aeb06ec86d1
Hoang Truong <tvhoang91@gmail.com>
2026-05-28 16:29:56 +0700

    fix onboard block

--- tests/e2e/bin/test-env-setup.sh
+++ tests/e2e/bin/test-env-setup.sh
@@ -25,5 +25,8 @@ wp-env run cli --config="$CONFIG" wp option update blogname 'YayCommerce E2E Tes
 echo -e 'Disable WooCommerce coming soon \n'
 wp-env run cli --config="$CONFIG" wp option update woocommerce_coming_soon no
 
+echo -e 'Skip WooCommerce onboarding wizard \n'
+wp-env run cli --config="$CONFIG" wp option update woocommerce_onboarding_profile '{"skipped":true}' --format=json
+
 echo -e 'Upload test images \n'
 wp-env run cli --config="$CONFIG" wp media import './test-data/images/image-01.png' './test-data/images/image-02.png' './test-data/images/image-03.png'
